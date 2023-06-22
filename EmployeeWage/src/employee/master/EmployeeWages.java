6755dc88bc5306ebc4c71daf503a859e7309b349
yami <yaminichaudhari2001@gmail.com>
2023-06-22 17:33:28 +0530

    employee UC6

--- EmployeeWage/src/employee/master/EmployeeWages.java
+++ EmployeeWage/src/employee/master/EmployeeWages.java
@@ -1,18 +1,26 @@
 package employee.master;
-//Add Part time
-//Employee & Wage
-//- Assume Part time Hour is 8
+//Calculate Wages till
+//a condition of total
+//working hours or
+//days is reached for
+//a month - Assume 100 hours
 public class EmployeeWages {
-    public static final int IS_PART_TIME=1;
-    public static final int IS_FULL_TIME=2;
-    public static final int EMP_RATE_PER_HOUR=20;
-    public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
+
     public static void main(String[] args) {
         int empHrs = 0;
         int empWage = 0;
+        int totalemphrs = 0;
+        int totalWorkingDays = 0;
         int totalempWage = 0;
         int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        while (totalemphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+
             switch (empcheck) {
                 case IS_PART_TIME: {
                     empHrs = 4;
@@ -26,10 +34,12 @@ public class EmployeeWages {
                     empHrs = 0;
 
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalempWage=+empWage;
         }
-        System.out.println("EmpWage " + empWage);
-        System.out.println("totalWage "+totalempWage);
+        totalemphrs = empHrs;
+        System.out.println("DAYS: " + totalWorkingDays + " total emp HRS " + empHrs);
+
+
+        totalempWage = totalemphrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total EMP Wage " + totalempWage);
     }
 }
